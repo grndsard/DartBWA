b8947a7d8c900ddbf51abb0d5cad9b99a7a9df3b
Grand <grandisardaf@gmail.com>
2023-10-12 14:40:01 +0700

    update fd

---
+++ fd.dart
@@ -0,0 +1,18 @@
+void main() { 
+  
+
+var name = 'Voyager I';
+var year = 1998;
+var antennaDiameter = 3.7;
+var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+var image = {
+  'tags': ['saturn'],
+  'url': '//path/to/saturn.jpg'
+};
+
+  print('Satellite Name : ${name}');
+  print(year);
+  print(antennaDiameter);
+  print(flybyObjects[1]);
+  print(image);
+  }
